5864948bdd3cf0a364d0b2878dc903fb510b3890
Ilias Azagagh <ilias.azagagh@hva.nl>
2019-04-12 17:18:49 +0200

    Initial Commit

---
+++ TrailerApp500729493/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrailerApp500729493
+//
+//  Created by Ilias Azagagh on 4/12/19.
+//  Copyright © 2019 Ilias Azagagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
